2bfad5f3635eecb1ae0b353c1570a93dc63688b2
Arlen <arlenbajgazakov@gmail.com>
2025-06-16 20:10:01 +0600

    lesson3

--- books/models.py
+++ books/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Books(models.Model):
@@ -20,3 +22,16 @@ class Books(models.Model):
     class Meta:
         verbose_name = 'книгу'
         verbose_name_plural = 'книг'
+
+
+
+class Reviews(models.Model):
+    choise_books = models.ForeignKey(Books,on_delete=models.CASCADE,related_name='book',verbose_name='выберите книгу',null=True)
+    text = models.TextField(verbose_name='напишите отзыв')
+    created_ad = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey( User,on_delete=models.CASCADE,null=True,blank=True,verbose_name='Автор')
+
+    mark = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],null=True,blank=True,verbose_name='Оценка (от 1 до 5)')
+
+    def __str__(self):
+        return f'{self.choise_books}-{self.text}'
